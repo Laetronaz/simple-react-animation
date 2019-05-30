10b8fc3d24fc56554d7ae6265f567327b9e691f7
Laetronaz <michelwarcraft@hotmail.com>
2019-05-29 21:00:08 -0400

    Finished Course

---
+++ src/components/component3.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function component3() {
+  return (
+    <div style={c3Style}>
+      <h1>component3</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+        soluta, sit aut expedita recusandae et cumque culpa minima esse, quod,
+        illum laborum a pariatur. Commodi sunt perferendis perspiciatis possimus
+        ex facere, eos quam aut at dolor animi officiis, quasi amet illum est
+        sint veritatis vitae. Nesciunt, modi veritatis soluta in amet eos ut
+        repellendus delectus saepe quos laborum ducimus pariatur nobis numquam.
+        Ad est veniam porro, neque laudantium impedit, quasi dolor quibusdam
+        repellat unde tempora.
+      </p>
+    </div>
+  );
+}
+
+const c3Style = {
+  background: "skyblue",
+  color: "white",
+  padding: "1.5rem 1.5rem 5rem 1.5rem"
+};
